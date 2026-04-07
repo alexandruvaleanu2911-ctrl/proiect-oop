47ec84da5a4dbe8799be1f42eed540846f9aa0d1
test123-oss <150047050+test123-oss@users.noreply.github.com>
2026-04-07 23:52:55 +0300

    sper ca merge!!

--- src/Example.cpp
+++ src/Example.cpp
@@ -3,12 +3,14 @@
 // This also works if you do not want `../`, but some editors might not like it
 // #include "Example.h"
 
-void Example::f() const {
-    std::cout << "private function f: " << x << "\n";
-}
-
-void Example::g() {
-    ++y;
-    f();
-    std::cout << "public function g: " << y << "\n";
-}
+/*
+// void Example::f() const {
+//     std::cout << "private function f: " << x << "\n";
+// }
+//
+// void Example::g() {
+//     ++y;
+//     f();
+//     std::cout << "public function g: " << y << "\n";
+// }
+*/
