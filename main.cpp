26acf5eb7beeb4ea8065505ebc8a30f77150026f
valexandrei <alexandruvaleanu2911@gmail.com>
2026-04-21 15:38:44 +0300

    cppcheck sper

--- main.cpp
+++ main.cpp
@@ -9,30 +9,30 @@
 
 int main() {
     std::string nume = "Erou";
-    int dim = 10;
-
-    JocDungeon joculMeu(nume, dim, dim);
+    JocDungeon joculMeu(nume, 10, 10);
     joculMeu.initSesiune();
 
     Jucator erou(nume, Pozitie(1, 1));
     erou.adaugaXP(50);
-
     erou.setPozitie(Pozitie(1, 1));
+
+    if (joculMeu.getLabirint().estePozitieValida(1, 1)) {
+        std::cout << "Pozitie confirmata de Labirint.\n";
+    }
 
     std::vector<Inamic*> listaInamici;
     listaInamici.push_back(new Inamic("Goblin", Pozitie(2, 2), 15));
 
     int l = joculMeu.getLabirint().getLinii();
     int c = joculMeu.getLabirint().getColoane();
-    if (l > 0 && c > 0) {
-        joculMeu.getLabirint().afisareGrafica(erou.getPozitie(), listaInamici);
-    }
+    std::cout << "Dimensiuni: " << l << "x" << c << "\n";
+
+    joculMeu.getLabirint().afisareGrafica(erou.getPozitie(), listaInamici);
 
     Inventar* rucsac = new Inventar(5);
     Pistoale* p = new Pistoale(20, 12);
     p->reincarca();
     rucsac->adaugaObiect(p);
-    rucsac->afiseazaTot();
     rucsac->folosesteToate();
 
     joculMeu.verificaInteractiune(erou, listaInamici);
